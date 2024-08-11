39ade09b744bed5f2ad2c55357839b466fadb80f
bchaffee23 <93299297+bchaffee23@users.noreply.github.com>
2024-08-11 01:25:32 -0400

    Create command_list.lua
    list of commands. prob dont need a sepeerate fle for this

---
+++ command_list.lua
@@ -0,0 +1,13 @@
+-- command_list.lua
+
+local all_commands = {
+    quarry = "quarry",
+    refuel = "refuel",
+    unload = "unload",
+    find = "find",
+    test = "test"
+    -- Additional commands
+}
+-- Export configuration
+return all_commands
+
